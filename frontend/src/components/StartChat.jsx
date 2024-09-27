68fd1b1c2746d792df38831e2cf55234986097cc
Nikunj Manhas <nikunj.manhas@kanerika.com>
2024-09-27 12:06:16 +0530

    Removed unrequired code and selected the first chat to show messages its messages first

--- frontend/src/components/StartChat.jsx
+++ frontend/src/components/StartChat.jsx
@@ -3,8 +3,8 @@ import logo from '../assets/kanerika_logo.jpg'
 function StartChat() {
   return (
     <div className='flex flex-col items-center p-6'>
-        <img src={logo} alt="kanerika-logo" width={100} />
-      <h1>Welcome/Start to chat</h1>
+        <img src={logo} alt="kanerika-logo" width={200} />
+      <h1 className='text-lg'>Hi, How can I help you?</h1>
     </div>
   )
 }
